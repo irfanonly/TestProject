3569a2ce531b743655a139821b5c908d33b24813
Irfan <mamirfan92@gmail.com>
2015-12-30 20:15:01 +0530

    MyProject

--- MyProject/MyProject/Properties/AssemblyInfo.cs
+++ MyProject/MyProject/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("MyProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyProduct("MyProject")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fc01ca99-4a3c-401e-90b9-8595a24cf859")]
+[assembly: Guid("2a3819e3-cb16-4f3e-9879-97ecc0a3dbbe")]
 
 // Version information for an assembly consists of the following four values:
 //
