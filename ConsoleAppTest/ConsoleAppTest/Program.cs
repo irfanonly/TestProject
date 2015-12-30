b9e085bbf3210465118beb9ad4bc6192f33a8723
Irfan <mamirfan92@gmail.com>
2015-12-30 18:35:51 +0530

    Hello World commit

--- ConsoleAppTest/ConsoleAppTest/Program.cs
+++ ConsoleAppTest/ConsoleAppTest/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleAppTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
